8bb1f4244c63234548caccaebbdb73caa71a06a0
Jean9LM <jeanninelmartinez@gmail.com>
2024-06-17 13:12:00 -0600

    Added format Date function

--- app.js
+++ app.js
@@ -5,14 +5,35 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#descript");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let dayElement = document.querySelector("#day");
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  dayElement.innerHTML = formatDate(date);
   windElement.innerHTML = response.data.wind.speed;
-  timeElement.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
   temperatureElement.innerHTML = `${Math.round(temperature)}&deg`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
