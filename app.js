eb554f6914722dacc5049fd4c8cfbe798529032b
Jean9LM <jeanninelmartinez@gmail.com>
2024-06-28 10:42:32 -0600

    Added forecast api and called get/display forecast function

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function refreshWeather(response) {
   windElement.innerHTML = response.data.wind.speed;
   temperatureElement.innerHTML = `${Math.round(temperature)}&deg`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -48,6 +49,17 @@ function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
+}
+
+function getForecast(city) {
+  let apiKey = "f2b35710c0850e450f427a44t4o3a3cc";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
 }
 
 let searchFormElement = document.querySelector("#search-form");
